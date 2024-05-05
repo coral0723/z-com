d09e1d9446d3fab57e84a049448841e5227737f3
san9901 <san990190@gmail.com>
2024-05-05 17:18:26 +0900

    두 번째 폴더 구조 세팅 완료

---
+++ src/app/(afterLogin)/home/layout.tsx
@@ -0,0 +1,10 @@
+import React, { ReactNode } from "react";
+
+export default async function HomeLayout({children}: {children: ReactNode}) {
+    return (
+        <div>
+            홈 레이아웃
+            {children}
+        </div>
+    )
+}
